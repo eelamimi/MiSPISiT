6009906318c03207c650700ab3c054a29709a76e
glebka <b.gleba77@gmail.com>
2026-02-09 11:01:04 +0300

    test of metric and section

--- model/question.py
+++ model/question.py
@@ -3,13 +3,13 @@ import json
 
 class Question:
     def __init__(self, q: tuple):
-        self.id = q[0]
-        self.text = q[1]
-        self.options = self.__get_options(q[2])
-        self.answer = q[3]
-        self.difficulty = q[4]
-        self.group = q[5]
-        self.type = q[6]
+        self.id: int = q[0]
+        self.text: str = q[1]
+        self.options: dict[int, str] = self.__get_options(q[2])
+        self.answer: int = q[3]
+        self.difficulty: int = q[4]
+        self.group: int = q[5]
+        self.type: int = q[6]
 
     def __str__(self):
         return f"{self.id} {self.text} {self.options} {self.answer} {self.difficulty} {self.group} {self.type}"
@@ -22,3 +22,6 @@ class Question:
 
     def __get_options(self, options):
         return json.loads(options, object_hook=self.__keys_to_int)
+
+    def is_correct_answer(self, option: int) -> bool:
+        return self.answer == option
--- window/questions_window.py
+++ window/questions_window.py
@@ -1,16 +1,20 @@
 import tkinter as tk
+from tkinter import messagebox as mb
 
 from .base import ChildWindow
+from model.question import Question
 
 
 class QuestionsWindow(ChildWindow):
-    def __init__(self, parent, questions, w=300, h=400):
+    def __init__(self, parent, questions: list[Question], w=300, h=400):
         super().__init__(parent, w, h)
         self.questions = questions
         self.index = 0
-        self.current_q = self.questions[self.index]
+        self.current_q = questions[self.index]
         self.type = self.current_q.type
         self.option = tk.IntVar(value=1)
+        self.correct_answers = 0
+        self.len = len(questions)
 
         self.grid_rowconfigure(0, weight=1)
         self.grid_rowconfigure(10, weight=1)
@@ -26,14 +30,14 @@ class QuestionsWindow(ChildWindow):
         self.text_label = tk.Label(self)
         self.text_label.grid(row=3, column=1, columnspan=2, pady=10)
 
-        self.first_option = tk.Radiobutton(self,  variable=self.option, value=1)
+        self.first_option = tk.Radiobutton(self, variable=self.option, value=1)
         self.first_option.grid(row=4, column=1, columnspan=2, pady=10)
-        self.second_option = tk.Radiobutton(self,  variable=self.option, value=2)
+        self.second_option = tk.Radiobutton(self, variable=self.option, value=2)
         self.second_option.grid(row=5, column=1, columnspan=2, pady=10)
-        self.third_option = tk.Radiobutton(self,  variable=self.option, value=3)
+        self.third_option = tk.Radiobutton(self, variable=self.option, value=3)
         self.third_option.grid(row=6, column=1, columnspan=2, pady=10)
 
-        self.next_q_btn = tk.Button(self, command=self.__next_q)
+        self.next_q_btn = tk.Button(self, text="Следующий вопрос", command=self.__next_q)
         self.next_q_btn.grid(row=7, column=2, pady=10)
 
         self.__update_q()
@@ -45,7 +49,7 @@ class QuestionsWindow(ChildWindow):
         self.center_window(self, new_w if new_w > 300 else 300, 400)
 
     def __increment_index(self):
-        if self.index < 2:
+        if self.index < (self.len - 1):
             self.index += 1
             self.current_q = self.questions[self.index]
 
@@ -55,12 +59,28 @@ class QuestionsWindow(ChildWindow):
         self.first_option.config(text=self.current_q.options[1])
         self.second_option.config(text=self.current_q.options[2])
         self.third_option.config(text=self.current_q.options[3])
-        if self.index < 2:
-            self.next_q_btn.config(text="Следующий вопрос")
-        else:
-            self.next_q_btn.config(text="Закончить тестирование", command=self.return_to_main_to_main)
+        if self.index >= (self.len - 1):
+            self.next_q_btn.config(text="Закончить тестирование", command=self.__stop_test)
         self.__update_width()
 
+    def __is_correct_answer(self):
+        if self.current_q.is_correct_answer(self.option.get()):
+            self.correct_answers += 1
+
     def __next_q(self):
+        self.__is_correct_answer()
         self.__increment_index()
         self.__update_q()
+
+    def __stop_test(self):
+        self.__is_correct_answer()
+        self.__save_test()
+        mb.showinfo(
+            title=f"Результат {self.type}",
+            message=f"Правильных ответов: {self.correct_answers}\n"
+                    f"Неправильных ответов: {self.len - self.correct_answers}\n\n"
+                    f"Баллов: {round(self.correct_answers / 3 * 10)}")
+        self.return_to_main_to_main()
+
+    def __save_test(self):
+        self.parent.save_results(self.correct_answers / 3 * 10, self.type)
